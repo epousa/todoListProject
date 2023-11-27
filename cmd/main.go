bf05ed06f5b63ee6e29320faf84e5634095ad923
Eduardo Pousa <pousaedu@protonmail.com>
2023-11-27 12:00:51 +0000

    structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// users info
+type user struct {
+	username string
+	password string
+}
+
+// tasks info
+type task struct {
+	id           int    //task 1,2,3,4,5 ...
+	todo         string //what to do
+	state        bool   //completed or not
+	timestampReg string //timestamp of registration
+	timestampCom string //timestamp of completion
+}
+
+// Array of users
+var users []user
+
+// Array of tasks
+var todoList []task
+
+// Each user corresponds an array of tasks
+// key -> user password hash / value -> todoList
+var database map[string][]task
 
 func main() {
 	fmt.Println("My todoListProject !")
