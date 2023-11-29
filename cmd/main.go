1cd10b86cf903d16de092f81c03acdc148938022
Eduardo Pousa <pousaedu@protonmail.com>
2023-11-29 13:41:18 +0000

    update

--- cmd/main.go
+++ cmd/main.go
@@ -14,13 +14,13 @@ func main() {
 	if len(users) == 0 {
 		username, password := view.InitView()
 		userOp.AddUser(&users, username, password)
-		userOp.AddUser(&users, username, password)
+		view.CleanStdout()
 	}
 
 	taskOp.AddTask(&users[0].TodoList, "workout")
 	taskOp.DoneTask(&users[0].TodoList, 0)
-	taskOp.AddTask(&users[1].TodoList, "workout")
-	taskOp.DoneTask(&users[1].TodoList, 0)
+	// taskOp.AddTask(&users[1].TodoList, "workout")
+	// taskOp.DoneTask(&users[1].TodoList, 0)
 	// fmt.Println(userOp.FindUser(users, 0))
 	// fmt.Println(userOp.FindUser(users, 1))
 
--- internal/view/view.go
+++ internal/view/view.go
@@ -5,21 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 
+	userOp "github.com/epousa/todoListProject/internal/user"
 	"gitlab.com/david_mbuvi/go_asterisks"
 )
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 
 func InitView() (string, []byte) {
 	//ask for admin user creation
 	fmt.Println("Welcome! Create an admin account to start.")
 	fmt.Printf("Enter your admin username: ")
 
-	reader := bufio.NewReader(os.Stdin)
 	username, err := reader.ReadString('\n')
 
 	if len(strings.TrimSpace(username)) == 0 {
-		err = fmt.Errorf("Your FirstName can't be empty %v", username)
+		err = fmt.Errorf("Your Username can't be empty %v", username)
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
@@ -33,5 +37,24 @@ func InitView() (string, []byte) {
 		fmt.Println(err.Error())
 	}
 
+	fmt.Printf("\n\n### You registered successfully! :) ###")
+
 	return username, password
+}
+
+func CleanStdout() {
+	time.Sleep(3 * time.Second)
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func AdminView(users *[]userOp.User) {
+	//ask for admin user creation
+
+}
+
+func StandardView(user userOp.User) {
+	//ask for admin user creation
+
 }
